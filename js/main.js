9848d6603adfe9dc918fbf41d8c53a87657745bf
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-20 19:34:24 +0000

    Rensponsive navbar for larger displays
    Removed hamburger, changed orientation and colour of navbar. To remove background word effect on larger displays.
    Having an issue however where background colour of nav does not change on scroll. Scroll is recognised however as alert will ping when tested.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
+// selecting elements
 const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobileMeunu = document.querySelector(".header .nav-bar .nav-list ul");
-const header = document.querySelector(".header .container");
+const menuItem = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const header = document.querySelector(".header.container");
 
-hamburger.addEventListener("click", ()=>{
+// toggle hamburger and mobile menu on clicking icon
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobileMeunu.classList.toggle("active");
 })
+
+// toggle hamburger and mobile menu on clicking menu items
+menuItem.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobileMeunu.classList.toggle('active');
+	});
+});
+
+// change colour of header upon scrolling in y position
+document.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 250){
+        header.style.backgroundColor = "29323c";
+	} else {
+		header.style.backgroundColor = "transparent";
+	}
+});
+
